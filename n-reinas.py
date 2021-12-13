694ee4054b874e883feffea99ae2bc78d6ef28c0
Maycol Flores <maycolfloresruiz06@gmail.com>
2021-12-12 19:36:14 -0600

    n-reinas.py: actualizado

--- n-reinas.py
+++ n-reinas.py
@@ -1,3 +1,6 @@
+# from tkinter import *
+# from tkinter import ttk
+
 x = 0
 
 def checkQueens(queens, n, k):
@@ -6,20 +9,40 @@ def checkQueens(queens, n, k):
             return False
     return True
 
-def NQueens(queens, n, k):
+def NQueens(queens, n, k, dictSolutions):
     if(k == n):
         global x
         x = x + 1
-        print(f'\nSolución: {x}: ', end="")
+        sol = []
+        # print(f'Solución: {x}: ', end="")
         for i in range(0, n):
-            print(f'{queens[i]} ', end="")
+            # print(f'{queens[i]} , ', end="")
+            sol.append(queens[i])
+        print("\n")
+        dictSolutions[x] = {
+            x : sol
+        }
     else:
         for queens[k] in range(0, n):
             if(checkQueens(queens, n, k)):
-                NQueens(queens, n, k+1)
+                NQueens(queens, n, k+1, dictSolutions)
+    return dictSolutions
 
+# root = Tk()
+# root.geometry('800x600')
+# root.title('N Reinas')
+# mainTitle = Label(root, text="Problema de las N Reinas")
+# mainTitle.pack() 
+# mainTitle.config(font=('Arial', 24))
+# entry = ttk.Entry(root)
+# entry.place(x=50, y=50)
+# ttk.Button(root, text='Salir', command=quit).pack(side=BOTTOM)
+# root.mainloop()
 
 k = 0
+dictSolutions = {}
 amount = int(input('\nIngrese la cantidad de reinas: '))
 queens = [-1 for n in range(0, amount)]
-NQueens(queens, amount, k)
+res = NQueens(queens, amount, k, dictSolutions)
+
+print(f'\n\n{res}\n\n')
