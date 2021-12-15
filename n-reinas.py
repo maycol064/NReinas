5c2d4f4a4121200d2bb203ef88a681e86497d45a
Maycol Flores <maycolfloresruiz06@gmail.com>
2021-12-15 12:45:41 -0600

    Comentarios agregados

--- n-reinas.py
+++ n-reinas.py
@@ -10,13 +10,22 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import os
 import sys
+
+# Declaración de variables globales
 x = 0
 a = 1
+
+''' Esta función recibe 3 parámetros, las reinas, el número de reinas y k. Y verifica si no son 
+iguales los elementos de las reinas'''
 def checkQueens(queens, n, k):
     for i in range(0, k):
         if((queens[i] == queens[k]) or (abs(k - i) == abs(queens[k] - queens[i]))):
             return False
     return True
+
+''' Recibe, las reinas, número de reinas, k y el diccionario de soluciones, verifia si k = n, para
+agregar el resultado en un array y el array en un diccionario, si no, hace una comprobación de la
+función anterior, para hacer una llamada recursiva de esta función. '''
 def NQueens(queens, n, k, dictSolutions):
     if(k == n):
         global x
@@ -31,11 +40,19 @@ def NQueens(queens, n, k, dictSolutions):
         for queens[k] in range(0, n):
             if(checkQueens(queens, n, k)):
                 NQueens(queens, n, k+1, dictSolutions)
+
+''' Función creadora de la interfaz '''
 def window():
+    # Definimos root como la ventana principal
     root = Tk()
+    # Le damos dimensiones
     root.geometry('1024x700')
+    # Le asignamos un título
     root.title('N Reinas')
+    # Evitamos que se redimensione
     root.resizable(0,0)
+
+    # Widgets que conforman la interfaz
     mainTitle = ttk.Label(root, text="Problema de las N Reinas")
     mainTitle.pack() 
     mainTitle.config(font=('Arial', 18))
@@ -46,28 +63,40 @@ def window():
     entryQueens = ttk.Entry(root)
     entryQueens.pack()
     entryQueens.config(font=('Arial', 12), justify=("center"))
+    # Estilos de los botones
     s = ttk.Style()
     s.configure(
         "MyButton.TButton",
         font=("Arial", 12),
         background="#000000"
     )
+    ''' Función para pintar el tablero, usamos matplotlib y un canvas de tkinter, recibe el número de
+    soluciones, el diccionario de solcuiones, el número de solución, la figura, el elemento de matplotlib
+    y la canva.
+    Por cada llamada en la variable array guardamos la solución que guardamos en el diccionario para
+    que se pueda acceder a los valores y pintar el tablero '''
     def printBoard(m, dictSolutions, a, fig, ax, canvas):
-        print(f'Solución {a}')
+        # print(f'Solución {a}')
         array = dictSolutions[a][a]
+        # Limpiamos el canva y quitamos los ejes
         ax.clear()
         ax.axis('off')
+        # Ciclo para iterar y dibujar el tablero
         for i in range(0, m+1):
             ax.plot([0, m], [i, i], color="gray")
             ax.plot([i, i], [m, 0], color="gray")
+        # Ciclo para colocar la reina
         for i in range(0, m):
             if array[i] != -1:
                 ax.plot([i+0.5], [array[i]+0.5], marker=".", markersize=30, color="black")
         plt.tight_layout()
         ax.patch.set_visible(False)
+        # Dibujar canva
         canvas.draw()
         canvas.get_tk_widget().pack()
         a += 1
+
+    # Función que agregar más widgets a la interfaz y comprueba si el número ingresado fue correcto
     def callQueens():
         k = 0
         global a
@@ -90,6 +119,7 @@ def window():
                 txtView = ttk.Entry(root)
                 txtView.pack()
                 txtView.config(font=('Arial', 12), justify=("center"))
+                # Verficia si el número es correcto para pintar más widgets y mandar a llamar la función del tablero
                 def verify(m, dictSolutions):
                     a = txtView.get()
                     if a.isdigit():
@@ -101,27 +131,35 @@ def window():
                             printBoard(m, dictSolutions, b, fig, ax, canvas)
                             txtView.delete("0","end")
                         else:
+                            # Mensajes de error
                             messagebox.showinfo(f'Mayor a 0', f'El número debe ser mayor 0 y menor a {x+1}')
                             txtView.delete("0","end")
                     else:
+                        # Mensajes de error
                         messagebox.showinfo('Sólo números', 'Sólo se permiten números.')
                         txtView.delete("0","end")
+                # Declaración del canvas
                 fig = Figure(frameon=False)
                 ax = fig.add_subplot(111)
                 canvas = FigureCanvasTkAgg(fig, root)
                 btnView = ttk.Button(root, text="Ver", style="MyButton.TButton", command=lambda:verify(m ,dictSolutions))
                 btnView.pack()
             else:
+                # Mensajes de error
                 messagebox.showinfo('Mayor a 3', "El número debe ser mayor a 3 y menor a 11")
                 entryQueens.delete("0","end")
         else:
+            # Mensajes de error
             messagebox.showinfo('Sólo números', 'Sólo se permiten números mayores a 3.')
             entryQueens.delete("0","end")
     btnQueens = ttk.Button(root, text="Generar", style="MyButton.TButton", command=callQueens)
     btnQueens.pack()
+    # Reincia la ventana para poder ingresar más datos
     def restart():
         os.execl(sys.executable, sys.executable, * sys.argv)
     btnExit = ttk.Button(root, text='Limpiar', command=restart, style="MyButton.TButton")
     btnExit.pack(side=BOTTOM)
+    # Convierte a root en la ventana principal
     root.mainloop()
+# Llamada de la interfaz
 window()
